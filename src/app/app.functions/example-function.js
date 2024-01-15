611b7ca084b15da29cd4d6aedd0b87f2c7bce846
Andrea Arestigue <andrea.arestigue@gmail.com>
2024-01-15 15:53:35 +0000

    V1 Attachment card

---
+++ src/app/app.functions/example-function.js
@@ -0,0 +1,92 @@
+const axios = require('axios');
+require("dotenv").config();
+
+async function updateAd(adId, properties, headers){
+
+  try{
+
+    const url = `https://api.hubapi.com/crm/v3/objects/2-116770344/${adId}`;
+    const data = properties;
+
+    const updateRequest = await axios.patch(url, data, headers);
+    return updateRequest;
+
+  }catch(err){
+    console.log(err);
+    throw err;
+  }
+}
+
+exports.main = async(context = {}, sendResponse) => {
+
+  const headers = {
+    headers: {
+      'Content-Type': 'application/json',
+      "Authorization": `Bearer ${process.env.HAPIKEY_JAHR_MEDIA}`,
+    }
+  }
+  
+  const docAttachment = context.parameters['attachment'];
+  const adId = context.parameters['adId'];
+  let payload = null;
+  let response= {};
+  let docStatus;
+
+  
+  
+
+  try {
+
+    if (docAttachment && docAttachment === "True"){
+      docStatus = "False";
+      payload ={
+        "properties": {
+          "druckunterlage_vorhanden_": "False"
+        } 
+      }
+    }else{
+      docStatus = "True";
+      payload ={
+        "properties":{
+          "druckunterlage_vorhanden_": "True"
+        }
+      }
+    }
+    
+    const updateProperty = await updateAd(adId, payload, headers);
+
+    if(updateProperty.status && updateProperty.status === 200 && docStatus === "False" || updateProperty.status && updateProperty.status === 201 && docStatus === "False" ){
+      response = {
+        updateMessage: "Attachment was removed succesfully!",
+        docAttached: docStatus,
+        buttonText: "Confirm Attachment"
+
+      }
+    }else if(updateProperty.status && updateProperty.status === 200 && docStatus === "True" || updateProperty.status && updateProperty.status === 201 && docStatus === "True"){
+      response = {
+        updateMessage: "Attachment was confirmed succesfully!",
+        docAttached: docStatus,
+        buttonText: "Remove confirmation"
+      }  
+    }else{
+      response = {
+        updateMessage: "There was an error. Please try again",
+        docAttached: docStatus,
+        buttonText: "Confirm Attachment"
+      }
+    }
+    console.log("status", updateProperty.status, response);
+
+    return response;
+
+    
+  } catch (error) {
+    console.log(error);
+    response = {
+      updateMessage: `There was an error. Please try again. Error: ${error.message}`,
+      docAttached: docStatus,
+      buttonText: "Confirm Attachment"
+    }
+    return response;
+  }
+};
---
+++ src/app/extensions/Example.jsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import {
+  Divider,
+  Link,
+  Button,
+  Text,
+  Input,
+  Flex,
+  hubspot,
+} from "@hubspot/ui-extensions";
+
+// Define the extension to be run within the Hubspot CRM
+hubspot.extend(({ context, runServerlessFunction, actions }) => (
+  <Extension
+    context={context}
+    runServerless={runServerlessFunction}
+    sendAlert={actions.addAlert}
+    fetchProperties={actions.fetchCrmObjectProperties}
+  />
+));
+
+// Define the Extension component, taking in runServerless, context, & sendAlert as props
+const Extension = ({ context, runServerless, sendAlert, fetchProperties }) => {
+  const [text, setText] = useState("");
+  const [docAttached, setDocAttached] = useState(null);
+  const [adId, setAdId] = useState("");
+  const [button, setButton] = useState("");
+  
+
+useEffect(() => {
+  fetchProperties(["druckunterlage_vorhanden_", "hs_object_id"])
+    .then(properties => {
+      setDocAttached(properties.druckunterlage_vorhanden_);
+      setAdId(properties.hs_object_id);
+      /* if(properties.druckunterlage_vorhanden_ === "True"){
+        setButton("Remove ");
+      }else{
+        setButton("Confirm");
+      } */
+  });
+}, [fetchProperties]);
+ 
+useEffect(() => {
+  
+      if(docAttached=== "True"){
+        setButton("Remove Attachment");
+      }else{
+        setButton("Confirm Attachment");
+      }
+
+}, [docAttached]);
+
+
+  // Call serverless function to execute with parameters.
+  // The `myFunc` function name is configured inside `serverless.json`
+  const handleClick = async () => {
+    const { response } = await runServerless({ name: "myFunc", parameters: { text: text, attachment: docAttached, adId: adId } });
+    setDocAttached(response.docAttached);
+    sendAlert({ message: response.updateMessage });
+
+  };
+
+
+  return (
+    <>
+      <Text>
+        <Text format={{ fontWeight: "bold" }}>
+          Update Attachment Property
+        </Text>
+       Please, click the button to confirm the ad file was attached or removed.
+       
+      </Text>
+      <Flex direction="row" align="end" gap="small">
+        <Button 
+        type="submit" 
+        onClick={handleClick}
+        variant="destructive"
+        size="md"
+        >
+          {button}
+        </Button>
+      </Flex>
+      <Divider />
+  
+    </>
+  );
+};
